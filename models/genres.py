ac3f7338c68585ad85c89bc35d6958e012e24eec
maribelaristizabal <maribelaristizabal079@gmail.com>
2023-05-24 16:45:05 -0500

    models/actor

---
+++ models/genres.py
@@ -0,0 +1,11 @@
+from sqlalchemy import column, Integer, String 
+
+from config.database import Base
+
+class Genres(Base):
+    
+    __tablename__ = "genres"
+    
+    id = column(Integer, primary_key = True)
+    gen_title = column(String)
+    
---
+++ models/movies_genres.py
@@ -0,0 +1,10 @@
+from sqlalchemy import column, ForeignKey, Integer
+
+from config import base 
+
+class movieGenres(base):
+    __tablename__ = "movie_genres"
+
+    id=column(interger,primary_key=True)
+    gen_id = column(Integer,ForeignKey("generes_id"))
+    movie_id = column(Integer,ForeignKey("movie.id"))
