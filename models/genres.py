9badd70b2d36d5c88e8a8b992a26e30567d2bfd4
Diego <diego.aguirre.9805@gmail.com>
2023-05-24 15:11:38 -0500

    Movie_Cast

---
+++ models/genres.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column
+
+from config.database import Base
+
+
+class Genres(Base)
+    
+    _tablename_="genres"
+
+    id= Column(Integer, primary_key=True)
+    gen_tittle = Column(string)
+
+
+
+
+
+
+
