5c11b8a15b9c2fcc173249901e092f9b01f15c78
nicolas <jaljordan77@gmail.com>
2023-05-25 11:56:02 -0500

    schema generes and movie genres

---
+++ schemas/movie_genres.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+
+from typing import Optional
+
+class MovieGenres(BaseModel):
+   id : Optional[int] = None
+   gen_id : int = Field(ge=1, description="id de genero")
+   movie_id :int = Field(ge=1, description="llave foranea de peliculas")
+
+   class Config:
+        schemas_extra = {
+            "example":{
+                "gen_id":2,
+                "movie_id":3
+            }
+        }
