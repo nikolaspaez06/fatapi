db722f458c433bc4bf202df44f6927347c0432a1
nicolas <jaljordan77@gmail.com>
2023-05-25 13:19:46 -0500

    actualizacion

---
+++ schemas/movie_director.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+
+from typing import Optional
+
+class Director(BaseModel):
+    id : Optional[int] = None
+    dir_id:int = Field(ge=1, description="id del director")
+    mov_id :int = Field(ge=1, description="llave foranea de la pelicula ")
+
+
+
+    class Config:
+        schemas_extra = {
+            "example":{
+                "dir_id":2,
+                "mov_id":3
+
+            }
+        }
