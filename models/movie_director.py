2951a72dee102b6364ec1c39469d7ddfebf2f385
nicolas <jaljordan77@gmail.com>
2023-05-27 20:09:52 -0500

    subiendo relacion y elimando archivo repetido

--- models/movie_director.py
+++ models/movie_director.py
@@ -8,3 +8,6 @@ class MovieDirector(Base):
     id=Column(Integer,primary_key=True)
     dir_id = Column(Integer,ForeignKey("dir_id"))
     movi_id = Column(Integer,ForeignKey("mov.id"))
+
+movie = relationship("movie", back_populates="movie_director")
+director = relationship("director", back_populates="movie_director")
