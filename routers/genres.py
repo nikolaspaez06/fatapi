dc66d97e49d7f302e4b3e928741136230b57abe9
unknown <santiagobenitez467@gmail.com>
2023-05-29 16:42:06 -0500

    Actualización

--- routers/genres.py
+++ routers/genres.py
@@ -32,6 +32,26 @@ def create_genres(genres:Genres):
     GenresService(db).create_genre(genres)
     return JSONResponse(content={"message":"genre created successfully", "status_code" : 201})
 
+@genres_router.put('/genres{id}', tags =['genres'])
+def update_genre(id:int,genres:Genres):
+    db = Session()
+    result = GenresService(db).get_for_id(id)
+    if not result:
+        return JSONResponse(content={"message":"genre don't gound", "status_code":404})
+    GenresService(db).update_genre(genres)
+    return JSONResponse(content={"message":"genre update successfully", "status_code": 202}, status_code=202)
+    
+@genres_router.delete('/genres{id}', tags =['genres'])
+def delete_genre(id:int):
+    db = Session()
+    result = GenresService(db).get_for_id(id)
+    if not result:
+        return JSONResponse(content={"message":"genre don't found", "status_code":404})
+    GenresService(db).delete_genre(id)
+    return JSONResponse(content={"message":"genre delete successfully", "status_code":200}, status_code=200)    
+    
+    
+
 
 #para el genres delete debemos verificar que el id existe y depués ese genero lo eliminamos
 
