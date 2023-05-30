0ed7e5fc06eb2aa7682484e1ccd30fb80af9e2a0
unknown <santiagobenitez467@gmail.com>
2023-05-30 15:45:58 -0500

    actualiza

--- models/rating.py
+++ models/rating.py
@@ -7,8 +7,8 @@ class Rating(Base):
     __tablename__ = "Rating"
     
     id = Column(Integer, primary_key=True )
-    mov_id = Column(Integer, ForeignKey("mov.id"))
-    rev_id = Column(Integer, ForeignKey("rev.id"))
-    rev_stars = Column(Integer, ForeignKey("rev.stars"))
-    num_o_ratings = Column(Integer, ForeignKey("num_o_ratings"))
+    mov_id = Column(Integer, ForeignKey("movie.id"))
+    rev_id = Column(Integer, ForeignKey("reviewer.id"))
+    rev_stars = Column(Integer)
+    num_o_ratings = Column(Integer)
     
