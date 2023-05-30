9bed4859b3904029436cd0c3bb93d6bb42cda7d9
nicolas <jaljordan77@gmail.com>
2023-05-30 09:53:14 -0500

    subir router movi director

---
+++ routers/movies_director.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter,Path, Query
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.encoders import jsonable_encoder
+
+from config.database import Session
+from service.movies_director import Mov_Director as movi_directorService
+from schemas.movie_director import Mov_Director as Mov_DirectorSchemas
+
+
+mov_director_router = APIRouter()
+
+@mov_director_router.get("/mov_director", tags=["mov_director"],status_code=200)
+def get_mov_director():
+    db=Session()
+    result = movi_directorService(db).get_mov_director()
+    return JSONResponse(content=jsonable_encoder(result), status_code=200)
+
+@mov_director_router.get("/mov_director_for_id", tags=["mov_director"],status_code=200)
+def get_mov_director_for_id(id:int):
+    db=Session()
+    result= movi_directorService(db).get_for_id(id)
+
+@mov_director_router.post("/mov_director", tags=["mov_director"],status_code=201)
+def create_mov_director(movie_director:Mov_DirectorSchemas):
+    db=Session()
+    movi_directorService(db).create_mov_director(movie_director)
+    return JSONResponse(content={"menssage": "mov_director created succesfully", "status_code": 201}, status_code= 201)
+
+@mov_director_router.put("/mov_director{id}", tags=["mov_director"])
+def update_mov_director(id:int,data:Mov_DirectorSchemas):
+    db=Session()
+    result=movi_directorService(db).get_for_id(id)
+    if not result:
+        return JSONResponse(content= {"message":"mov_director don't found", "status_code":404})
+    movi_directorService(db).update_mov_director(data)
+    return JSONResponse(content={"message":"mov_actor updated succesfully", "status_code":200}, status_code=200)
+
+@mov_director_router.delete("/mov_director{id}", tags=["mov_director"])
+def delete_mov_director(id:int,data:Mov_DirectorSchemas):
+    db=Session()
+    result=movi_directorService(db).get_for_id(id)
+    if not result:
+        return JSONResponse(content= {"message":"mov_director don't gound", "status_code":404})
+    movi_directorService(db).delete_mov_director(data)
+    return JSONResponse(content={"message":"mov_actor delete succesfully", "status_code":200}, status_code=200)
+
+
