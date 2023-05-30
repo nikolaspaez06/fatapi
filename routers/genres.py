f5a684f67cc536965611da6fac3b5dff74ee27b9
huxley <elhuxley277@gmail.com>
2023-05-30 14:12:12 -0500

    subiendo director sevice y router

--- routers/genres.py
+++ routers/genres.py
@@ -24,7 +24,7 @@ def get_genre_for_id(id:int):
     db = Session()
     result = GenresService(db).get_for_id(id)
     return JSONResponse(content=jsonable_encoder(result),status_code=200)
-   
+
 
 @genres_router.post('/genres', tags=['genres'], status_code=201)
 def create_genres(genres:Genres):
@@ -32,6 +32,27 @@ def create_genres(genres:Genres):
     GenresService(db).create_genre(genres)
     return JSONResponse(content={"message":"genre created successfully", "status_code" : 201})
 
+@genres_router.put("/genres{id}",tags=["genres"])
+def update_genre(id:int,genres:Genres):
+    db = Session()
+    result = GenresService(db).get_for_id(id)
+    if not result:
+        return JSONResponse(content={"message":"genre dont't found", "status_code":404})
+    GenresService(db).update_genre(genres)
+    return JSONResponse(content={"message":"genre update succeefully","status_code":202}, status_code=202)
+    
+    #todo editar
+    
+@genres_router.delete("/genres{id}",tags=["genres"])
+def delete_genre(id:int,movie:Genres):
+    db = Session()
+    result = GenresService(db).get_for_id(id)
+    if not result:
+        return JSONResponse(content={"message":"genre dont't found", "status_code":404})
+    GenresService(db).delete_genre(id)
+    return JSONResponse(content={"message":"genre delete successfully", "status_code":200}, status_code=200)
+    #todoeliminar
+    
 
 #para el genres delete debemos verificar que el id existe y depués ese genero lo eliminamos
 
